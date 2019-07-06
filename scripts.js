f7bd344ad0b3d1428fd2d3f14d2a1be31dc9f3c6
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2019-07-06 20:07:03 +0100

    Enhanced the random fings!

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   setTheRandomFing();
-}
+};
 
 function setTheRandomFing() {
-  return fetch('randomfings.json')
+  return fetch("randomfings.json")
     .then(function(response) {
       return response.json();
     })
@@ -11,8 +11,22 @@ function setTheRandomFing() {
       let randomFings = data.randomFings;
       let numberOfFings = randomFings.length;
       let aTrulyRandomFing = Math.floor(Math.random() * numberOfFings);
-    
-      document.getElementById('random-fing').textContent = randomFings[aTrulyRandomFing].text;
+
+      // Set the "random fing" text
+      document.getElementById("random-fing").textContent =
+        randomFings[aTrulyRandomFing].text;
+
+      // Sets the title attribute if the "random fing" is related to a demoscene production
+      if (randomFings[aTrulyRandomFing].production != undefined) {
+        document.getElementById("random-fing").title =
+          randomFings[aTrulyRandomFing].production +
+          " by " +
+          randomFings[aTrulyRandomFing].releasedBy +
+          " [" +
+          randomFings[aTrulyRandomFing].platform +
+          " " +
+          randomFings[aTrulyRandomFing].releaseYear +
+          "]";
+      }
     });
 }
-          
