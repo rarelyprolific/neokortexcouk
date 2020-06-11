a9193c9702c2773b6292e2e58217ec2c0b52e35e
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2020-06-11 20:06:28 +0100

    Added random neokortex logos

--- site/scripts.js
+++ site/scripts.js
@@ -1,7 +1,8 @@
 // Initialises any required JavaScript at page load
 // (..until we get something more structured in here)
-window.onload = function() {
+window.onload = function () {
   setTheRandomFing();
+  setRandomNeokortexLogo();
 
   // Runs the collyviewer (..if we're on a page which includes the collyviewer)
   if (typeof displayCollyModemStyle !== "undefined") {
@@ -12,17 +13,16 @@ window.onload = function() {
 // Displays a new random fing each the page is loaded
 function setTheRandomFing() {
   return fetch("randomfings.json")
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
       let randomFings = data.randomFings;
       let numberOfFings = randomFings.length;
       let aTrulyRandomFing = Math.floor(Math.random() * numberOfFings);
 
       // Set the "random fing" text
-      document.getElementById("random-fing").textContent =
-        randomFings[aTrulyRandomFing].text;
+      document.getElementById("random-fing").textContent = randomFings[aTrulyRandomFing].text;
 
       // Sets the title attribute if the "random fing" is related to a demoscene production
       if (randomFings[aTrulyRandomFing].production != undefined) {
@@ -42,4 +42,12 @@ function setTheRandomFing() {
 // Switches the Amiga display font on the old ASCII viewer
 function changeAsciiFont(fontFamily) {
   document.getElementsByTagName("pre")[0].style.fontFamily = fontFamily;
+}
+
+// Sets a random Neokortex logo
+function setRandomNeokortexLogo() {
+  const arrayOfLogos = ["img/neokortex-logo-1.jpg", "img/neokortex-logo-2.png"];
+  const randomLogoIndex = Math.floor(Math.random() * arrayOfLogos.length);
+
+  document.getElementById("neokortex-logo").setAttribute("src", arrayOfLogos[randomLogoIndex]);
 }
