b6430ec24928209bb684579a34d93725a5622ae0
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2022-05-07 21:11:11 +0100

    Added randomfing and juggled nkx logos

--- site/scripts.js
+++ site/scripts.js
@@ -53,6 +53,7 @@ function setRandomNeokortexLogo() {
     "img/neokortex-logo-3.png",
     "img/neokortex-logo-4.png",
     "img/neokortex-logo-5.png",
+    "img/neokortex-logo-6.png",
   ];
   const randomLogoIndex = Math.floor(Math.random() * arrayOfLogos.length);
 
