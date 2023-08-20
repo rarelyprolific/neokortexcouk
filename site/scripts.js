5071ece5cdf249a079bd6b09d39b40c1535b4aea
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2023-08-20 16:56:10 +0100

    Refactoring JSON data and javascript

--- site/scripts.js
+++ site/scripts.js
@@ -1,9 +1,6 @@
 // Initialises any required JavaScript at page load
 // (..until we get something more structured in here)
 window.onload = function () {
-  setTheRandomFing();
-  setRandomNeokortexLogo();
-
   buildPartyResultsMenuItems();
   buildReleasesMenuItems();
 
@@ -13,6 +10,10 @@ window.onload = function () {
   }
 
   const nkxWebApp = new NkxWebApp();
+
+  // Displays a new neokortex logo and random fing each the page is loaded
+  nkxWebApp.setRandomNeokortexLogo();
+  nkxWebApp.setTheRandomFing();
 
   // Populates the version number and moniker in the page footer.
   document.getElementById("version-number").innerHTML = nkxWebApp.getVersionNumber();
@@ -27,35 +28,48 @@ class NkxWebApp {
   getVersionMoniker() {
     return "[virtuallynojavascriptimprovements]";
   }
-}
 
-// Displays a new random fing each the page is loaded
-function setTheRandomFing() {
-  return fetch("randomfings.json")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      let randomFings = data.randomFings;
-      let numberOfFings = randomFings.length;
-      let aTrulyRandomFing = Math.floor(Math.random() * numberOfFings);
+  setRandomNeokortexLogo() {
+    const arrayOfLogos = [
+      "img/neokortex-logo-1.jpg",
+      "img/neokortex-logo-2.png",
+      "img/neokortex-logo-3.png",
+      "img/neokortex-logo-4.png",
+      "img/neokortex-logo-5.png",
+      "img/neokortex-logo-6.png",
+    ];
+    const randomLogoIndex = Math.floor(Math.random() * arrayOfLogos.length);
 
-      // Set the "random fing" text
-      document.getElementById("random-fing").textContent = randomFings[aTrulyRandomFing].text;
+    document.getElementById("neokortex-logo").setAttribute("src", arrayOfLogos[randomLogoIndex]);
+  }
 
-      // Sets the title attribute if the "random fing" is related to a demoscene production
-      if (randomFings[aTrulyRandomFing].production != undefined) {
-        document.getElementById("random-fing").title =
-          randomFings[aTrulyRandomFing].production +
-          " by " +
-          randomFings[aTrulyRandomFing].releasedBy +
-          " [" +
-          randomFings[aTrulyRandomFing].platform +
-          " " +
-          randomFings[aTrulyRandomFing].releaseYear +
-          "]";
-      }
-    });
+  setTheRandomFing() {
+    return fetch("data/randomfings.json")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        let randomFings = data.randomFings;
+        let numberOfFings = randomFings.length;
+        let aTrulyRandomFing = Math.floor(Math.random() * numberOfFings);
+
+        // Set the "random fing" text
+        document.getElementById("random-fing").textContent = randomFings[aTrulyRandomFing].text;
+
+        // Sets the title attribute if the "random fing" is related to a demoscene production
+        if (randomFings[aTrulyRandomFing].production != undefined) {
+          document.getElementById("random-fing").title =
+            randomFings[aTrulyRandomFing].production +
+            " by " +
+            randomFings[aTrulyRandomFing].releasedBy +
+            " [" +
+            randomFings[aTrulyRandomFing].platform +
+            " " +
+            randomFings[aTrulyRandomFing].releaseYear +
+            "]";
+        }
+      });
+  }
 }
 
 // Switches the Amiga display font on the old ASCII viewer
@@ -63,25 +77,10 @@ function changeAsciiFont(fontFamily) {
   document.getElementsByTagName("pre")[0].style.fontFamily = fontFamily;
 }
 
-// Sets a random Neokortex logo
-function setRandomNeokortexLogo() {
-  const arrayOfLogos = [
-    "img/neokortex-logo-1.jpg",
-    "img/neokortex-logo-2.png",
-    "img/neokortex-logo-3.png",
-    "img/neokortex-logo-4.png",
-    "img/neokortex-logo-5.png",
-    "img/neokortex-logo-6.png",
-  ];
-  const randomLogoIndex = Math.floor(Math.random() * arrayOfLogos.length);
-
-  document.getElementById("neokortex-logo").setAttribute("src", arrayOfLogos[randomLogoIndex]);
-}
-
 // Builds the party releases section of the navigation menu (a cheap and nasty way because I got fed
 // up tweaking multiple files but didn't want to pull in excess stuff just to do a HTML include!)
 function buildPartyResultsMenuItems() {
-  return fetch("partyresults.json")
+  return fetch("data/partyresults.json")
     .then(function (response) {
       return response.json();
     })
@@ -100,7 +99,7 @@ function buildPartyResultsMenuItems() {
 // Builds the releases section of the navigation menu (a cheap and nasty way because I got fed
 // up tweaking multiple files but didn't want to pull in excess stuff just to do a HTML include!)
 function buildReleasesMenuItems() {
-  return fetch("releases.json")
+  return fetch("data/releases.json")
     .then(function (response) {
       return response.json();
     })
