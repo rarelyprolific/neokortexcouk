85226f3c5d712546b14a96abb07e180e49ca0a00
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2025-09-29 20:03:10 +0100

    Adding colly line counter function to phase out lineheight query string parameter

--- site/scripts/collyviewer.js
+++ site/scripts/collyviewer.js
@@ -1,8 +1,7 @@
 // Displays an ASCII or ANSI colly from the "collys" folder based on the filename
 // specified in the "collyname" parameter (e.g. "/site/collyviewer.html?collyname=DLine_GeoMetriX.ans.txt")
-// and with the height specified in the "lineheight" parameter
 // as if it were being downloaded on a modem at 57600 baud.
-function displayCollyModemStyle() {
+async function displayCollyModemStyle() {
   // Process the querystring parameters
   var querystringParams = new URLSearchParams(window.location.search);
   var collyName = querystringParams.get("collyname");
@@ -14,6 +13,15 @@ function displayCollyModemStyle() {
 
   // Set the base directory to load collys from
   var collyPath = "collys/" + collyName;
+
+  // Count the number of lines in the colly if lineheight isn't specified in the querystring
+  // (Still need to figure out the line counter function for some types of ANSI files which are not CRLF line endings.)
+  if (lineHeight > 0) {
+    var lineCount = lineHeight;
+  } else {
+    var lineCount = await countLinesFromFileAsync(collyPath);
+  }
+
 
   // Load and display the colly
   var controller = AnsiLove.animate(
@@ -30,7 +38,8 @@ function displayCollyModemStyle() {
       font: font || "topazplus",
       bits: displayMode || "8",
       icecolors: 0,
-      rows: lineHeight,
+      // Sets the height (i.e. number of lines) of the colly and adds a few extra blank rows for formatting.
+      rows: lineCount + 5,
       extendedcolumns: extendedColumns || 0,
       disableblink: disableblink || "false",
     }
@@ -49,4 +58,14 @@ function collyviewerFontChanger(font) {
   // Set the "font" parameter and reload the page.
   querystringParams.append("font", font);
   window.location.href = window.location.pathname + "?" + querystringParams.toString();
+}
+
+// Counts the number of lines in the colly to calculate the height.
+async function countLinesFromFileAsync(pathToColly) {
+  const response = await fetch(pathToColly);
+
+  const text = await response.text();
+  const lines = text.split(/\r?\n/);
+
+  return lines.length;
 }
